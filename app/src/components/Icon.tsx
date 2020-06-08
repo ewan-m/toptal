4cd1777024e6fa43adf01199f39ee8ccdd5f9b14
Ewan Morrison <ewanmorrrison@gmail.com>
2020-06-08 14:06:11 +0100

    Add material icons with utility component

---
+++ app/src/components/Icon.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+import "./Icon.scss";
+
+interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
+	withMargin?: "left" | "right";
+}
+
+export const Icon: FunctionComponent<IconProps> = ({
+	children,
+	withMargin,
+	className,
+	...props
+}) => (
+	<span
+		{...props}
+		className={`material-icons ${
+			withMargin ? ` material-icons--${withMargin} ` : ""
+		}${className ? className : ""}`}
+	>
+		{children}
+	</span>
+);
