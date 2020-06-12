8bc6e4494d10d581ded0dd3839afb7bc35a2d4d5
Ewan Morrison <ewanmorrrison@gmail.com>
2020-06-12 18:07:22 +0100

    UI - Update packages and add recoil for state management

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserHistory } from "history";
 import * as React from "react";
 import { render } from "react-dom";
+import { RecoilRoot } from "recoil";
 import { Route, Router, Switch } from "react-router-dom";
 import { SiteContainer } from "./components/SiteContainer";
 import "./index.scss";
@@ -15,18 +16,20 @@ import { Four04 } from "./pages/404";
 const history = createBrowserHistory();
 
 const Index = () => (
-	<Router history={history}>
-		<SiteContainer>
-			<Switch>
-				<Route path="/" exact component={Landing} />
-				<Route path="/sign-in" exact component={SignIn} />
-				<Route path="/sign-up" exact component={SignUp} />
-				<Route path="/magic-link" exact component={MagicLink} />
-				<Route path="/dashboard" exact component={Dashboard} />
-				<Route component={Four04} />
-			</Switch>
-		</SiteContainer>
-	</Router>
+	<RecoilRoot>
+		<Router history={history}>
+			<SiteContainer>
+				<Switch>
+					<Route path="/" exact component={Landing} />
+					<Route path="/sign-in" exact component={SignIn} />
+					<Route path="/sign-up" exact component={SignUp} />
+					<Route path="/magic-link" exact component={MagicLink} />
+					<Route path="/dashboard" exact component={Dashboard} />
+					<Route component={Four04} />
+				</Switch>
+			</SiteContainer>
+		</Router>
+	</RecoilRoot>
 );
 
 render(<Index />, document.getElementById("root"));
